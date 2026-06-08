98d18b59962ef12e4fe917577b335f4961e8937c
SaberLS <patryk.platek19@gmail.com>
2026-06-08 11:16:52 +0200

    init

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      {/* <AcmeLogo /> */}
+    </div>
+  )
+}
+
+export default Page
