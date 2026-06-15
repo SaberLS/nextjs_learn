73a0e08112b7dd1e212788f6b5db8448d9685ed8
SaberLS <patryk.platek19@gmail.com>
2026-06-15 20:13:37 +0200

    chapter 7

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,6 +1,9 @@
 import { LatestInvoice } from '@/app/lib/definitions'
 import { lusitana } from '@/app/ui/fonts'
 import { ArrowPathIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import Image from 'next/image'
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
@@ -14,7 +17,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -23,7 +26,7 @@ export default async function LatestInvoices({
                   'flex flex-row items-center justify-between py-4',
                   {
                     'border-t': i !== 0,
-                  },
+                  }
                 )}
               >
                 <div className="flex items-center">
@@ -49,9 +52,9 @@ export default async function LatestInvoices({
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
